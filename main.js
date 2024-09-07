4c2ab88938e5b87bdaac88630966a8bb27ea891f
WCroW1997 <wcrow1997@gmail.com>
2024-09-07 11:31:47 +0300

    update product

--- main.js
+++ main.js
@@ -7,3 +7,35 @@ menuBtn.addEventListener('click', function () {
     menu.classList.toggle('active');
     lock.classList.toggle('lock');
 })
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
+    },
+});
